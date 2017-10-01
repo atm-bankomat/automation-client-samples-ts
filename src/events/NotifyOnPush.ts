417f12a8dd4ca758bb86d4786247bef7f9a0d9e1
Christian Dupuis <cd@atomist.com>
2017-10-01 23:52:34 +0200

    some fixes

--- src/events/NotifyOnPush.ts
+++ src/events/NotifyOnPush.ts
@@ -1,13 +1,16 @@
 import * as GraphQL from "@atomist/automation-client/graph/graphQL";
+import { Failure } from "@atomist/automation-client/HandlerResult";
 import {
     EventFired,
     EventHandler,
     HandleEvent,
     HandlerContext,
     HandlerResult,
+    Success,
     Tags,
 } from "@atomist/automation-client/Handlers";
 import { logger } from "@atomist/automation-client/internal/util/logger";
+import * as _ from "lodash";
 import * as graphql from "../typings/types";
 
 @EventHandler("Notify channel on push", GraphQL.subscriptionFromFile("graphql/push"))
@@ -15,13 +18,16 @@ import * as graphql from "../typings/types";
 export class NotifyOnPush implements HandleEvent<graphql.PushWithRepo.Subscription> {
 
     public handle(e: EventFired<graphql.PushWithRepo.Subscription>, ctx: HandlerContext): Promise<HandlerResult> {
-        logger.info(`Incoming event is %s`, JSON.stringify(e.data, null, 2));
-
-        return Promise.all(e.data.Push.map(p =>
-            ctx.messageClient.addressChannels(`Got a push with sha \`${p.after.sha}\``,
-                p.repo.channels.map(c => c.name))))
-            .then(() => {
-                return Promise.resolve({ code: 0 });
-            });
+        return Promise.all(e.data.Push.map(p => {
+                const channels = _.get(p, "repo.channels") as graphql.PushWithRepo.Channels[];
+                if (channels && channels.length > 0) {
+                    return ctx.messageClient.addressChannels(`Got a push with sha \`${p.after.sha}\``,
+                        channels.map(c => c.name));
+                } else {
+                    return Success;
+                }
+            }))
+            .then(() => Success)
+            .catch(() => Failure);
     }
 }
